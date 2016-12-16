cee513c37f0ca3750537874ca4657c8af37a9dc1
Usuario Local <Usuario Local>
2016-12-16 17:49:27 +0100

    Version 1.0 - Plantilla Inicial con código práctica 5

---
+++ src/js/gameover_scene.js
@@ -0,0 +1,36 @@
+var GameOver = {
+    create: function () {
+        console.log("Game Over");
+
+        var button = this.game.add.button(400, 300, 'button', this.restart, this, 2, 1, 0);
+        button.anchor.set(0.5);
+        
+        var goText = this.game.add.text(400, 100, "GameOver");
+        var text = this.game.add.text(0, 0, "Reset Game");
+        var rText = this.game.add.text (0, 0, "Return Main Menu");
+        goText.font ='Sniglet';
+        text.font = 'Sniglet';
+        rText.font = 'Sniglet';
+        text.anchor.set(0.5);
+        goText.anchor.set(0.5);
+        rText.anchor.set(0.5);
+        button.addChild(text);
+        
+        //TODO 8 crear un boton con el texto 'Return Main Menu' que nos devuelva al menu del juego.
+        var button2 = this.game.add.button(400, 200, 'button', this.returnMenu, this, 2, 1, 0);
+        button2.anchor.set(0.5);
+        button2.addChild(rText);
+    },
+    
+    //TODO 7 declarar el callback del boton.
+    returnMenu: function() {
+      this.game.state.start('menu');
+    },
+
+    restart: function () {
+        this.game.state.start('preloader');      
+    }
+    
+};
+
+module.exports = GameOver;
---
+++ src/js/menu_scene.js
@@ -0,0 +1,29 @@
+var MenuScene = {
+
+    preload : function()
+    {
+      console.log("preload de menu scene");
+      this.game.stage.backgroundColor = "#000000";    
+    },
+    
+    create: function () {
+        
+      var logo = this.game.add.sprite(this.game.world.centerX, this.game.world.centerY,'logo');
+      logo.anchor.setTo(0.5, 0.5);
+        
+      var buttonStart = this.game.add.button(this.game.world.centerX, this.game.world.centerY, 'button', this.actionOnClick, this, 2, 1, 0);
+      buttonStart.anchor.set(0.5);
+       
+      var textStart = this.game.add.text(0, 0, "Start");
+      textStart.font = 'Sniglet';
+      textStart.anchor.set(0.5);
+      buttonStart.addChild(textStart);
+
+    },
+    
+    actionOnClick: function(){
+      this.game.state.start('preloader');
+    } 
+};
+
+module.exports = MenuScene;
